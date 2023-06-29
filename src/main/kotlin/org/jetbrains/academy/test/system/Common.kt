356f6fdf9ae0b092c868df4e1c331120f02ac63a
Ilya Vlasov <ilya.vlasov@jetbrains.com>
2023-06-29 11:52:46 +0400

    fix: deprecate `Int.getVisibility` due to duplication

--- src/main/kotlin/org/jetbrains/academy/test/system/Common.kt
+++ src/main/kotlin/org/jetbrains/academy/test/system/Common.kt
@@ -8,13 +8,17 @@ fun Type.getShortName() = if ("<" in this.toString()) {
     val types = this.toString().split("<").toMutableList()
     types[0].getShortName()
 } else {
-    this.toString().lowercase().split(".").last()
+    this.toString().getShortName()
 }
 
 fun String.getShortName() = lowercase().split(".").last()
 
 fun throwInternalLibError(): Nothing = error("Internal library error!")
 
+@Deprecated(
+    "This extensions function is deprecated due to duplication",
+    replaceWith = ReplaceWith("getVisibility()", imports = ["org.jetbrains.academy.test.system.models.getVisibility"])
+)
 fun Int.getVisibility(): Visibility? {
     if (Modifier.isPublic(this)) {
         return Visibility.PUBLIC
