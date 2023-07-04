abe6a6a6cf630155318dd20a431df93550cfccb4
anastasia.birillo <anastasia.i.birillo@gmail.com>
2023-07-04 18:49:56 +0200

    Upgrade gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,22 +4,57 @@ plugins {
     kotlin("jvm") version "1.8.20"
     id("io.gitlab.arturbosch.detekt") version "1.21.0"
     `maven-publish`
+    id("tanvd.kosogor") version "1.0.13"
 }
 
 group = "org.jetbrains.academy.test.system"
 version = "2.0.0"
 
-repositories {
-    mavenCentral()
-}
+allprojects {
+    apply {
+        plugin("kotlin")
+        plugin("io.gitlab.arturbosch.detekt")
+        plugin("maven-publish")
+    }
 
-dependencies {
-    implementation(kotlin("reflect"))
-    val junitJupiterVersion = "5.9.0"
-    implementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")
-    testRuntimeOnly("org.junit.platform:junit-platform-console:1.9.2")
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        implementation(kotlin("reflect"))
+        val junitJupiterVersion = "5.9.0"
+        implementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+        testImplementation("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")
+        testRuntimeOnly("org.junit.platform:junit-platform-console:1.9.2")
+    }
+
+    tasks.test {
+        useJUnitPlatform()
+    }
+
+    kotlin {
+        jvmToolchain(11)
+    }
+
+    apply<io.gitlab.arturbosch.detekt.DetektPlugin>()
+
+    configure<io.gitlab.arturbosch.detekt.extensions.DetektExtension> {
+        config = rootProject.files("detekt.yml")
+        buildUponDefaultConfig = true
+        debug = true
+    }
+
+    val detektReportMerge by tasks.registering(io.gitlab.arturbosch.detekt.report.ReportMergeTask::class) {
+        output.set(rootProject.buildDir.resolve("reports/detekt/merge.sarif"))
+    }
+
+    tasks.withType<io.gitlab.arturbosch.detekt.Detekt> {
+        finalizedBy(detektReportMerge)
+        reports.sarif.required.set(true)
+        detektReportMerge.get().input.from(sarifReportFile)
+    }
 }
 
 fun getLocalProperty(key: String, file: String = "local.properties"): String? {
@@ -31,32 +66,6 @@ fun getLocalProperty(key: String, file: String = "local.properties"): String? {
         ?: println("File $file with properties not found")
 
     return properties.getProperty(key, null)
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
-kotlin {
-    jvmToolchain(11)
-}
-
-apply<io.gitlab.arturbosch.detekt.DetektPlugin>()
-
-configure<io.gitlab.arturbosch.detekt.extensions.DetektExtension> {
-    config = rootProject.files("detekt.yml")
-    buildUponDefaultConfig = true
-    debug = true
-}
-
-val detektReportMerge by tasks.registering(io.gitlab.arturbosch.detekt.report.ReportMergeTask::class) {
-    output.set(rootProject.buildDir.resolve("reports/detekt/merge.sarif"))
-}
-
-tasks.withType<io.gitlab.arturbosch.detekt.Detekt> {
-    finalizedBy(detektReportMerge)
-    reports.sarif.required.set(true)
-    detektReportMerge.get().input.from(sarifReportFile)
 }
 
 val spaceUsername = getLocalProperty("spaceUsername")
