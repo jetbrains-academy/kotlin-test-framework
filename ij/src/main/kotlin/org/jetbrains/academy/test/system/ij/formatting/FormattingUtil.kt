e20232dcf8d8107496e828335c59747aebbc1d7f
anastasia.birillo <anastasia.i.birillo@gmail.com>
2023-07-16 10:02:33 +0200

    Refactor code a bit, bump version

--- ij/src/main/kotlin/org/jetbrains/academy/test/system/ij/formatting/FormattingUtil.kt
+++ ij/src/main/kotlin/org/jetbrains/academy/test/system/ij/formatting/FormattingUtil.kt
@@ -2,15 +2,11 @@
 
 package org.jetbrains.academy.test.system.ij.formatting
 
-import com.intellij.codeInsight.daemon.impl.DaemonProgressIndicator
-import com.intellij.codeInspection.InspectionManager
-import com.intellij.codeInspection.LocalInspectionTool
-import com.intellij.codeInspection.ProblemDescriptor
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.command.WriteCommandAction
-import com.intellij.openapi.progress.ProgressManager
 import com.intellij.psi.PsiFile
 import com.intellij.psi.codeStyle.CodeStyleManager
+import org.jetbrains.academy.test.system.inspections.applyInspections
 import org.jetbrains.kotlin.idea.inspections.KotlinUnusedImportInspection
 
 // TODO: make it possible to check different aspects of formatting
@@ -24,25 +20,8 @@ fun PsiFile.checkIfFormattingRulesWereApplied() {
     assert(originalCode.trimIndent() == formattedCode.trimIndent()) { "The code after formatting should be:${System.lineSeparator()}$formattedCode${System.lineSeparator()}Please, apply code formatting refactoring to the code." }
 }
 
-fun PsiFile.checkIfInspectionsWerePassed(inspections: List<LocalInspectionTool>): Boolean {
-    val problems: MutableList<ProblemDescriptor> = mutableListOf()
-    val inspectionManager = InspectionManager.getInstance(project)
-    ProgressManager.getInstance().executeProcessUnderProgress(
-        {
-            for (inspection in inspections) {
-                problems.addAll(
-                    ApplicationManager.getApplication().runReadAction<List<ProblemDescriptor>> {
-                        inspection.processFile(this, inspectionManager)
-                    })
-            }
-        },
-        DaemonProgressIndicator()
-    )
-    return problems.isEmpty()
-}
-
 fun PsiFile.checkIfOptimizeImportsWereApplied() {
-    assert(this.checkIfInspectionsWerePassed(listOf(KotlinUnusedImportInspection()))) {
+    assert(applyInspections(listOf(KotlinUnusedImportInspection())).isEmpty()) {
         "Please, apply \"Optimize import\" option when formatting code."
     }
 }
