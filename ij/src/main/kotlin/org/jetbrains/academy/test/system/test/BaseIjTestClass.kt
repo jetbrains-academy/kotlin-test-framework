5945ef2be60c54c0c823c1df8f8a507ee3c7b990
Anna Potriasaeva <anchouls@pc-13-172.customer.ask4.lan>
2023-07-19 20:03:34 +0200

    Added two ij tests

--- ij/src/main/kotlin/org/jetbrains/academy/test/system/test/BaseIjTestClass.kt
+++ ij/src/main/kotlin/org/jetbrains/academy/test/system/test/BaseIjTestClass.kt
@@ -1,8 +1,46 @@
 package org.jetbrains.academy.test.system.test
 
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiRecursiveElementWalkingVisitor
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import org.jetbrains.kotlin.psi.KtBlockExpression
+import org.jetbrains.kotlin.psi.KtNamedFunction
+import org.jetbrains.kotlin.psi.KtProperty
 
 /**
  * A Base test class to create tests with PSI without adding intellij dependency into courses directly.
  */
-open class BaseIjTestClass : BasePlatformTestCase()
+open class BaseIjTestClass : BasePlatformTestCase() {
+
+    fun getMethodsContainingContent(content: String): List<String> {
+        val methodNames = mutableListOf<String>()
+        var currentMethod = ""
+        ApplicationManager.getApplication().runReadAction<Unit?> {
+            myFixture.file.accept(object : PsiRecursiveElementWalkingVisitor() {
+                override fun visitElement(element: PsiElement) {
+                    if (element is KtNamedFunction) currentMethod = element.name.toString()
+                    if (element is KtBlockExpression && element.text.contains(content))
+                        methodNames.add(currentMethod)
+                    super.visitElement(element)
+                }
+            })
+        }
+        return methodNames
+    }
+
+    fun existsConstantWithTheValue(elementValue: String): Boolean {
+        var existsConstant = false
+        ApplicationManager.getApplication().runReadAction<Unit?> {
+            myFixture.file.accept(object : PsiRecursiveElementWalkingVisitor() {
+                override fun visitElement(element: PsiElement) {
+                    if (element is KtProperty && element.modifierList?.text?.contains("const") == true) {
+                        if (element.text.contains(elementValue)) existsConstant = true
+                    }
+                    super.visitElement(element)
+                }
+            })
+        }
+        return existsConstant
+    }
+}
---
+++ ij/src/test/kotlin/org/jetbrains/academy/test/system/ij/formatting/BaseIjTestClassTests.kt
@@ -0,0 +1,46 @@
+package org.jetbrains.academy.test.system.ij.formatting
+
+import org.jetbrains.academy.test.system.test.BaseIjTestClass
+
+class BaseIjTestClassTests : BaseIjTestClass() {
+
+    fun testMethodsContainingContent() {
+        val example = """
+            class ExampleClass {
+
+                fun method1() {
+                    val actions = "Some actions"
+                    println("Content")
+                    println(actions)
+                }
+                
+                fun method2() {
+                    val content = "Content"
+                    val actions = "Some actions"
+                    println(actions + content)
+                }
+                
+                fun method3() {
+                    println("Content")
+                }
+            }
+        """.trimIndent()
+        setUp()
+        myFixture.configureByText("Task.kt", example)
+        val content = "println(\"Content\")"
+        assert(listOf("method1", "method3").equals(getMethodsContainingContent(content)))
+    }
+
+    fun testExistsConstant() {
+        val example = """
+            private const val CONSTANT1 = "some text"
+            private val notConstant = 0.5
+            const val CONSTANT2 = 2
+        """.trimIndent()
+        setUp()
+        myFixture.configureByText("Task.kt", example)
+        assert(existsConstantWithTheValue("\"some text\""))
+        assert(existsConstantWithTheValue("2"))
+        assert(!existsConstantWithTheValue("0.5"))
+    }
+}
