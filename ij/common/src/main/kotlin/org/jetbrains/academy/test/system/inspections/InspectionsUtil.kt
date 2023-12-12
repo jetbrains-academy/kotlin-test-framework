219755e6f48f33df9065d20a540620a79363e82d
anchouls <potryasaeva.anna@mail.ru>
2023-12-12 13:59:41 +0100

    Removed the check for Global Inspections

--- ij/common/src/main/kotlin/org/jetbrains/academy/test/system/inspections/InspectionsUtil.kt
+++ ij/common/src/main/kotlin/org/jetbrains/academy/test/system/inspections/InspectionsUtil.kt
@@ -1,17 +1,9 @@
-@file:Suppress("ForbiddenComment")
-
 package org.jetbrains.academy.test.system.inspections
 
 import com.intellij.codeInsight.daemon.impl.DaemonProgressIndicator
 import com.intellij.codeInspection.InspectionManager
 import com.intellij.codeInspection.LocalInspectionTool
 import com.intellij.codeInspection.ProblemDescriptor
-import com.intellij.codeInspection.GlobalSimpleInspectionTool
-import com.intellij.codeInspection.ProblemsHolder
-import com.intellij.codeInspection.ProblemDescriptionsProcessor
-import com.intellij.codeInspection.CommonProblemDescriptor
-import com.intellij.codeInspection.ex.GlobalInspectionContextBase
-import com.intellij.codeInspection.reference.RefEntity
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.progress.ProgressManager
 import com.intellij.psi.PsiFile
@@ -31,36 +23,4 @@ fun PsiFile.applyLocalInspections(inspections: List<LocalInspectionTool>): List<
         DaemonProgressIndicator()
     )
     return problems
-}
-
-fun PsiFile.applyGlobalInspections(inspections: List<GlobalSimpleInspectionTool>): Boolean {
-    // TODO: `checkFile` doesn't work due to `resolve` doesn't work
-    val inspectionManager = InspectionManager.getInstance(project)
-    val problemDescriptionsProcessor = MyProblemDescriptionsProcessor()
-    val globalContext = GlobalInspectionContextBase(project)
-    val problemsHolder = ProblemsHolder(inspectionManager, this, true)
-    ProgressManager.getInstance().executeProcessUnderProgress(
-        {
-            for (inspection in inspections) {
-                ApplicationManager.getApplication().runReadAction<Unit> {
-                    inspection.checkFile(this, inspectionManager, problemsHolder, globalContext, problemDescriptionsProcessor)
-                }
-            }
-        },
-        DaemonProgressIndicator()
-    )
-    return !problemsHolder.hasResults()
-}
-
-class MyProblemDescriptionsProcessor: ProblemDescriptionsProcessor {
-
-    private val problems: MutableList<CommonProblemDescriptor?> = mutableListOf()
-
-    override fun addProblemElement(refEntity: RefEntity?, vararg commonProblemDescriptors: CommonProblemDescriptor?) {
-        problems.addAll(commonProblemDescriptors)
-    }
-
-    fun getProblems(): MutableList<CommonProblemDescriptor?> {
-        return problems
-    }
 }
